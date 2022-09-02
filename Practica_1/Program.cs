89ed9fd83efa25c556d18d7b03bab9ccea8d1776
Irlanda <Irlanda@DESKTOP-18H9VR3>
2022-09-01 19:18:06 -0700

    Agregar archivos de proyecto.

---
+++ Practica_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Irlanda Maldonado Velez,Mexicali,23");
+            Console.ReadKey();
+                
+        }
+    }
+}
